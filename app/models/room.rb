ae35d5bc5dbcfebb450575a84061c122b311058f
SpenSer Bray <matthewqa@gmail.com>
2016-02-29 20:15:03 -0800

    Change Room relationship to "owner" instead.

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,5 @@
 class Room < ActiveRecord::Base
-  belongs_to :user
+  belongs_to :owner, class_name: 'User', foreign_key: "user_id"
   has_many :users, through: :passports
   has_many :messages
   
