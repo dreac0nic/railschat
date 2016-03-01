1be12dd0eafebe9ffba96bd756f2d538dcd99779
SpenSer Bray <matthewqa@gmail.com>
2016-02-29 20:18:42 -0800

    Update schema with new table names.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301010451) do
+ActiveRecord::Schema.define(version: 20160301035116) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "content"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20160301010451) do
     t.integer  "room_id"
   end
 
-  create_table "room_passports", force: :cascade do |t|
+  create_table "passports", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "room_id"
     t.datetime "created_at", null: false
